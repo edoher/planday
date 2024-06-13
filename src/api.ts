65deda568e6e38d7f089181eb946e062aa83fb5a
Eduardo Hernandez <hernandezmedia@gmail.com>
2024-06-13 13:24:25 +0200

    fix pagination indexes

--- src/api.ts
+++ src/api.ts
@@ -14,7 +14,7 @@ const api = async ({ page, per_page }: ApiRequest): Promise<ApiResponse> => {
     let processedData: GridElement[] = [];
 
     if (page && per_page) {
-        const start = page * per_page - 1;
+        const start = page === 1 ? 0 : (page - 1) * per_page;
         const end = start + per_page;
 
         processedData = data.slice(start, end);
